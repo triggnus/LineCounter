6a7f2bc8a90f9ffa751cda009b4d8f173da93c01
rob <somethingobscure@gmail.com>
2025-02-06 20:16:06 -0600

    improved method for determining the amount of column padding.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 //! # LineCounter
-//! 
-//! Simple file line counter. This program was written from scratch. Though it replicates the effect of wc -l \[fname\], it 
+//!
+//! Simple file line counter. This program was written from scratch. Though it replicates the effect of wc -l \[fname\], it
 //! contains no code in common.
 //!
 //! usage: lc \[file name(s)\]
-//! 
+//!
 //! Author: Rob Teeple <somethingobscure@gmail.com>
-//! 
+//!
 //! This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//! 
+//!
 //! This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 //! of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//! 
+//!
 //! You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu
 //! .org/licenses/>.
 
